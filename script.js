555869d580cb3cbce4696d258e2ed5bd7608efcc
gigijeskai <luigindaco@gmail.com>
2023-01-09 17:31:55 +0100

    commit

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let result = eval(document.getElementById('display').value)
 return document.getElementById('display').value = result
 }
 
+// function error(){
+//   document.getElementById("display"). value = 
+// }
+
 function deleteDisplay(){
 document.getElementById("display").value = null
 }
